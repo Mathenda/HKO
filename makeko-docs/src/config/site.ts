7ac2c017137b93739769525a1233b8ff479ed16d
Ennis Maphasha <ennism@bcr.co.za>
2026-03-12 15:56:56 +0200

    construct

--- makeko-docs/src/config/site.ts
+++ makeko-docs/src/config/site.ts
@@ -1,4 +1,4 @@
 // Site configuration
 export const siteConfig = {
-  underConstruction: false, // Set to false to make the site live
+  underConstruction: true, // Set to false to make the site live
 };
