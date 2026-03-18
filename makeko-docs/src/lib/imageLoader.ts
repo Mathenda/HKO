6ef4a3315ee27523542366bce083a9a03330e88e
Ennis Maphasha <ennism@bcr.co.za>
2026-03-18 16:28:10 +0200

    blob fix

--- makeko-docs/src/lib/imageLoader.ts
+++ makeko-docs/src/lib/imageLoader.ts
@@ -3,5 +3,6 @@ type ImageLoaderProps = { src: string; width: number; quality?: number };
 const BLOB_BASE_URL = "https://qiqe4rknb11or570.public.blob.vercel-storage.com";
 
 export default function blobImageLoader({ src }: ImageLoaderProps): string {
+  if (src.startsWith("http")) return src;
   return `${BLOB_BASE_URL}${src}`;
 }
